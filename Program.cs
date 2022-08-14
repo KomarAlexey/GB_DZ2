65bdc13e69fa3d75ac17d6a4f77d8fe8327c60be
Alex <angrylexa@mail.ru>
2022-08-14 23:10:52 +0300

    DZ3

--- Program.cs
+++ Program.cs
@@ -30,3 +30,25 @@
 // int thirdnum = num % 10;
 // Console.WriteLine($"Третья цифра числа {num} -> {thirdnum}");
 
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if ((num > 7) || (num <1 ))
+// {
+//     Console.WriteLine($"Неверный день недели");
+//     return;
+// }
+// if (num > 5)
+// {
+//     Console.WriteLine($"Да");
+// }
+// else
+// {
+//     Console.WriteLine($"нет");
+// }
