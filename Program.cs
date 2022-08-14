efa4b0dc788c22e5043e59982d3fe15fe82202b1
Alex <angrylexa@mail.ru>
2022-08-14 23:01:28 +0300

    DZ2

--- Program.cs
+++ Program.cs
@@ -11,3 +11,22 @@
 // int secondnum = num / 10 % 10;
 
 // Console.WriteLine($"Вторая цифра в числе {num} -> {secondnum}");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 100)
+// {
+//     Console.WriteLine($"Третий цифры нет");
+//     return;
+// }
+// int thirdnum = num % 10;
+// Console.WriteLine($"Третья цифра числа {num} -> {thirdnum}");
+
